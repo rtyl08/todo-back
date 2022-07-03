9650da71160ad7dc1f2647545094af0b233f1be8
rtyl08 <r.tylikowski@gmail.com>
2022-07-03 12:14:48 +0200

    Added express-rate-limit

--- index.ts
+++ index.ts
@@ -2,9 +2,16 @@ import express from "express"
 import cors from "cors";
 import 'express-async-errors';
 import {handleError} from "./utils/errors";
+import rateLimit from "express-rate-limit";
 
 const app = express();
-
+const limiter = rateLimit({
+    windowMs:15*60*1000,
+    max:100,
+    standardHeaders:true,
+    legacyHeaders:false,
+})
+app.use(limiter);
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
